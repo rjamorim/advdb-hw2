59cf0019c52380d38e6b6ea511ec094a921cfabb
pff2108 <pff2108@columbia.edu>
2015-10-26 04:45:19 -0400

    read and stored top4 urls for each query (FIFA.txt). now using file instead of using Bing queries while developing content summary.

--- qprober.py
+++ qprober.py
@@ -30,73 +30,116 @@ def run_query(query):
     content = response.read()
     # content contains the xml/json response from Bing.
     tree = json.loads(content)
-    return int(tree['d']['results'][0]['WebTotal'])
+
+    return tree['d']['results'][0]['Web'], int(tree['d']['results'][0]['WebTotal'])
 
 
-def process_final_coverage(coverage, specificity_parent):
-    global category
-    winner = ''
-    max_coverage = 0
-    sum_coverage = 0
-    for key in coverage.keys():
-        results = coverage[key]
-        sum_coverage += coverage[key]
-        if results > max_coverage:
-            max_coverage = results
-            winner = key
-    for key in coverage.keys():
-        print 'Specificity for category ' + str(key) + ' is ' + str(specificity_parent * coverage[key] / sum_coverage)
-        print 'Coverage for category ' + str(key) + ' is ' + str(coverage[key])
-    # Normalization to calculate specificity
-    specificity = (float(max_coverage) / sum_coverage) * specificity_parent
-    if specificity > t_es and max_coverage > t_ec:
-        category += '/' + winner
+class DatabaseClassifier(object):
 
+    def __init__(self):
+        self.coverage = defaultdict(int)
+        self.sum_coverage = 0
 
-def process_sub_list(coverage):
-    global category
-    winner = ''
-    max_coverage = 0
-    sum_coverage = 0
-    for key in coverage.keys():
-        results = coverage[key]
-        sum_coverage += coverage[key]
-        if results > max_coverage:
-            max_coverage = results
-            winner = key
-    for key in coverage.keys():
-        print 'Specificity for category ' + str(key) + ' is ' + str(float(coverage[key]) / sum_coverage)
-        print 'Coverage for category ' + str(key) + ' is ' + str(coverage[key])
-    # Normalization to calculate specificity
-    specificity = float(max_coverage) / sum_coverage
-
-    coverage = defaultdict(int)
-    if specificity > t_es and max_coverage > t_ec:
-        category += '/' + winner
+    def process_root_list(self):
         # try:
-        with open(winner.lower() + '.txt') as f:
+        with open('root.txt') as f:
+            i = 1
             for line in f:
                 value = line.split(' ', 1)
-                coverage[value[0]] += run_query(value[1].strip())
-            process_final_coverage(coverage, specificity)
+                cat = value[0]
+                current_query = value[1].strip()
+                result_top4, result_count = run_query(current_query)
+                self.coverage[cat] += result_count
+                self.sum_coverage += result_count
+                for result in result_top4:
+                    output_text.write('root\t%i\t%s\n' % (i, result['Url']))
+                    output_text.flush()
+                i += 1
+            self.process_sub_list()
         # except IOError:
         #    print 'List file not located. Processing will stop'
         #    exit(1)
 
+    def process_sub_list(self):
+        global category
+        winner = ''
+        max_coverage = 0
+        for key in sorted(self.coverage.keys()):
+            result_count = self.coverage[key]
+            print 'Specificity for category ' + str(key) + ' is ' + str(float(result_count) / self.sum_coverage)
+            print 'Coverage for category ' + str(key) + ' is ' + str(result_count)
+            if result_count > max_coverage:
+                max_coverage = result_count
+                winner = key
+        # Normalization to calculate specificity
+        specificity = float(max_coverage) / self.sum_coverage
 
-def process_root_list():
-    coverage = defaultdict(int)
-    # try:
-    with open('root.txt') as f:
-        for line in f:
-            value = line.split(' ', 1)
-            coverage[value[0]] += run_query(value[1].strip())
-        process_sub_list(coverage)
-    # except IOError:
-    #    print 'List file not located. Processing will stop'
-    #    exit(1)
+        self.coverage = defaultdict(int)
+        self.sum_coverage = 0
+        if specificity > t_es and max_coverage > t_ec:
+            category += '/' + winner
+            # try:
+            with open(winner.lower() + '.txt') as f:
+                i = 1
+                for line in f:
+                    value = line.split(' ', 1)
+                    cat = value[0]
+                    current_query = value[1].strip()
+                    result_top4, result_count = run_query(current_query)
+                    self.coverage[cat] += result_count
+                    self.sum_coverage += result_count
+                    for result in result_top4:
+                        output_text.write('%s\t%i\t%s\n' % (winner, i, result['Url']))
+                        output_text.flush()
+                    i += 1
+                self.process_final_coverage(specificity)
+            # except IOError:
+            #    print 'List file not located. Processing will stop'
+            #    exit(1)
+
+    def process_final_coverage(self, spec_parent):
+        global category
+        winner = ''
+        max_coverage = 0
+        for key in sorted(self.coverage.keys()):
+            result_count = self.coverage[key]
+            print 'Specificity for category ' + str(key) + ' is ' + str(spec_parent * result_count / self.sum_coverage)
+            print 'Coverage for category ' + str(key) + ' is ' + str(result_count)
+            if result_count > max_coverage:
+                max_coverage = result_count
+                winner = key
+        # Normalization to calculate specificity
+        specificity = (float(max_coverage) / self.sum_coverage) * spec_parent
+        if specificity > t_es and max_coverage > t_ec:
+            category += '/' + winner
 
 
-print 'Classifying for website ' + site + '\n'
-process_root_list()
-print '\nClassification for ' + site + ': ' + category
+class ContentSummarizer(object):
+
+    def __init__(self):
+        self.url_list = defaultdict(set)
+        self.url_read = defaultdict(int)
+
+    def read_file(self, filename):
+        # Read stored file...
+        with open(filename) as f:
+            for line in f:
+                value = line.strip().split('\t')
+                cat = value[0]
+                i = value[1]
+                url = value[2]
+                if self.url_read[url] == 0:
+                    self.url_read[url] = 1
+                    self.url_list[(cat, i)].add(url)
+
+
+# print 'Classifying for website ' + site + '\n'
+# output_text = file('FIFA.txt', 'w')
+# db_classifier = DatabaseClassifier()
+# db_classifier.process_root_list()
+# print '\nClassification for ' + site + ': ' + category
+
+c_summarizer = ContentSummarizer()
+c_summarizer.read_file('FIFA.txt')
+for category, count in sorted(c_summarizer.url_list.keys()):
+    print category, count, len(c_summarizer.url_list[(category, count)]), c_summarizer.url_list[(category, count)]
