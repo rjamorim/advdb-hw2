f77220a5b02e802849aadfdc6fb19b333e7a5b19
pff2108 <pff2108@columbia.edu>
2015-10-26 21:16:49 -0400

    content summary working (not hard-coded anymore)

--- qprober.py
+++ qprober.py
@@ -9,15 +9,15 @@ import json
 import subprocess
 from collections import defaultdict
 
-
-site = 'fifa.com'
+name = 'yahoo'
+site = name + '.com'
 t_es = 0.6
 t_ec = 100
 
 
 def run_query(query):
     # Execute query
-    # print 'Query being executed: ' + query
+    # print 'Query being executed: ' + query + ' (' + site
     query_url = urllib2.quote("'site:" + site + " " + query + "'")
     bing_url = 'https://api.datamarket.azure.com/Bing/SearchWeb/v1/Composite?Query='+query_url+'&$top=4&$format=json'
     account_key = 'hTvGEgXTQ8lDLYr8nnHocn7n9GSwF5antgnogEhNDTc'
@@ -40,21 +40,26 @@ class DatabaseClassifier(object):
         self.coverage = defaultdict(int)
         self.sum_coverage = 0
         self.category = ['Root']
+        self.probe_count = defaultdict(int)
+        self.url_list = defaultdict(list)
 
     def process_root_list(self):
+        winner = 'Root'
         # try:
-        with open('root.txt') as f:
+        with open(winner.lower() + '.txt') as f:
             i = 1
             for line in f:
                 value = line.split(' ', 1)
                 cat = value[0]
                 current_query = value[1].strip()
                 result_top4, result_count = run_query(current_query)
+                self.probe_count[winner] += 1
                 self.coverage[cat] += result_count
                 self.sum_coverage += result_count
                 for result in result_top4:
                     output_text.write('Root\t%i\t%s\n' % (i, result['Url']))
                     output_text.flush()
+                    self.url_list[(winner, i)].append(result['Url'])
                 i += 1
             self.process_sub_list()
         # except IOError:
@@ -86,11 +91,13 @@ class DatabaseClassifier(object):
                     cat = value[0]
                     current_query = value[1].strip()
                     result_top4, result_count = run_query(current_query)
+                    self.probe_count[winner] += 1
                     self.coverage[cat] += result_count
                     self.sum_coverage += result_count
                     for result in result_top4:
                         output_text.write('%s\t%i\t%s\n' % (winner, i, result['Url']))
                         output_text.flush()
+                        self.url_list[(winner, i)].append(result['Url'])
                     i += 1
                 self.process_final_coverage(specificity)
             # except IOError:
@@ -120,13 +127,17 @@ class ContentSummarizer(object):
         self.word_count_sub = defaultdict(int)
         self.url_list = defaultdict(list)
         self.url_read = defaultdict(int)
-        self.categories = ['Root', 'Sports', 'Soccer']
+        self.categories = []
         self.probe_count = defaultdict(int)
-        self.probe_count['Root'] = 66
-        self.probe_count['Sports'] = 24
+
+    def load_classifier(self, classifier):
+        # Load information form classifier..
+        self.probe_count = classifier.probe_count
+        self.categories = classifier.category
+        self.url_list = classifier.url_list
 
     def load_file(self, filename):
-        # Read stored file...
+        # Read stored file..
         with open(filename) as f:
             for line in f:
                 value = line.strip().split('\t')
@@ -139,7 +150,7 @@ class ContentSummarizer(object):
         for category in self.categories[1::-1]:
             print '\nCreating Content Summary for: ' + category
             for count in range(1, self.probe_count[category] + 1):
-                listing = self.url_list[(category, str(count))]
+                listing = self.url_list[(category, count)]
                 if listing:
                     print str(count) + '/' + str(self.probe_count[category])
                     for url in listing:
@@ -204,20 +215,16 @@ class ContentSummarizer(object):
                             self.word_count_sub[word] += 1
 
 
-# print 'Classifying for website ' + site + '\n'
-# output_text = file('FIFA.txt', 'w')
-# db_classifier = DatabaseClassifier()
-# db_classifier.process_root_list()
-# print '\n\nClassification for ' + site + ': ' + '/'.join(db_classifier.category)
-
-output_text_2 = file('FIFA_summary.txt', 'w')
-output_text_3 = file('FIFA_summary_sub.txt', 'w')
+print 'Classifying for website ' + site + '\n'
+output_text = file(name + '.txt', 'w')
+db_classifier = DatabaseClassifier()
+db_classifier.process_root_list()
+print '\n\nClassification for ' + site + ': ' + '/'.join(db_classifier.category)
 
 print '\n\n\nExtracting topic content summaries...'
+output_text_2 = file(name + '_summary.txt', 'w')
+output_text_3 = file(name + '_summary_sub.txt', 'w')
 c_summarizer = ContentSummarizer()
-c_summarizer.load_file('FIFA.txt')
+# c_summarizer.load_file(name + '.txt')
+c_summarizer.load_classifier(db_classifier)
 c_summarizer.summary()
-
-# Problemas:
-# 1) Processar cada url somente uma vez, mesmo que ela apareca no top4 para mais de uma probe query.
-# Mas eh preciso garantir que a url vai ser considerada na contagem para a subcategoria tambem.
